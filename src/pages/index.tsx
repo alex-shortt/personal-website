39a2b4d81726d6a1063fc54554cf19b90d3390c6
Alexander Shortt <alexander.shortt@gmail.com>
2025-05-15 09:32:02 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,8 @@ const COLOR: { [key in TYPE]: string } = {
 
 export const dynamic = "force-dynamic";
 
+
+
 const START = new Date("2015-01-02");
 const END = () => new Date(); // now
 const SCALE = 900; // px
